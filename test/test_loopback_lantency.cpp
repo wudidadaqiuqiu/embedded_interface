32f28fa53e7442d48089bf0eaac8aceb838c91fa
xy <wudidadaqiuqiu@qq.com>
2025-01-03 13:56:45 +0000

    回环测试通过

--- test/test_loopback_lantency.cpp
+++ test/test_loopback_lantency.cpp
@@ -16,22 +16,22 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "test_can_loopback_lantency");
     ros::NodeHandle nh;
     
-    LatencyTest latency_test(nh, "can1", "test_can_lantency", "test_can_lantency_recv", 1, 2);
-    Connector<ConnectorType::CAN> connector("can0");
-    ConnectorSingleRecvNode<ConnectorType::CAN, CanFrame> crn(connector);
-    ConnectorSendNode<ConnectorType::CAN, CanFrame> crn1(connector);
+    LatencyTest latency_test(nh, "can0", "test_can_lantency", "test_can_lantency_recv", 1, 2);
+    // Connector<ConnectorType::CAN> connector("can0");
+    // ConnectorSingleRecvNode<ConnectorType::CAN, CanFrame> crn(connector);
+    // ConnectorSendNode<ConnectorType::CAN, CanFrame> crn1(connector);
 
-    auto pub = nh.advertise<CanFrame::MSGT>("test_can_lantency_cli", 10);
-    auto l = [&pub](const CanFrame::MSGT& msg) {
-        if (msg.id == 1) {
-            std::cout << "recv: " << msg.id << std::endl;
-            CanFrame::MSGT out;
-            out.id = 2;
-            pub.publish(out);
-        }
-    };
-    crn.register_callback(l);
-    auto sub = nh.subscribe("test_can_lantency_cli", 10, decltype(crn1)::get_callback(), &crn1);
+    // auto pub = nh.advertise<CanFrame::MSGT>("test_can_lantency_cli", 10);
+    // auto l = [&pub](const CanFrame::MSGT& msg) {
+    //     if (msg.id == 1) {
+    //         std::cout << "recv: " << msg.id << std::endl;
+    //         CanFrame::MSGT out;
+    //         out.id = 2;
+    //         pub.publish(out);
+    //     }
+    // };
+    // crn.register_callback(l);
+    // auto sub = nh.subscribe("test_can_lantency_cli", 10, decltype(crn1)::get_callback(), &crn1);
     ros::spin();
     return 0;
 }
